42685676a5041d1e7d8a65c58a740dee902227b3
kreiv <kreivenastomas78@gmail.com>
2023-07-14 22:10:37 +0300

    .

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 const companyNameElement = document.getElementById('company-name');
 companyNameElement.textContent = 'Žvakės';
 
+const footerTextElement = document.getElementById('footer-text');
+footerTextElement.textContent = 'Puslapį kūrė Tomas Kreivenas ';
+
+const copyrightElement = document.getElementById('copyright');
+const currentYear = new Date().getFullYear();
+const copyrightText = `Visos Teisės Saugomos CodeAcademy &copy; ${currentYear}`;
+copyrightElement.innerHTML = copyrightText;
+
 function zoomIn(image) {
   image.classList.add('zoomed');
 }
